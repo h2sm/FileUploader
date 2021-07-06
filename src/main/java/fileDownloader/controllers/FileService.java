e054aea3d3be8a6fb7d49a21a3ec796a06357f13
Egor Fedorenko <fedorenko-e@kiszi.ru>
2021-07-06 18:08:07 +0300

    Spring boot type of loader

---
+++ src/main/java/fileDownloader/controllers/FileService.java
@@ -0,0 +1,32 @@
+package fileDownloader.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileService {
+
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
+
+    public void uploadFile(MultipartFile file) {
+        try {
+            Path copyLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            System.out.println(copyLocation + " COPYLOCATION");
+            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new FileStorageException("File Not Found");
+        }
+    }
+}
